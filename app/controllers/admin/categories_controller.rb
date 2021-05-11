a0da0647fd8ce40719843fe9291b1c1df73e4ab5
yuya <yuyaya777@gmail.com>
2021-05-11 14:55:54 +0000

    カテゴリーのコントローラー記述

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,32 @@
 class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
