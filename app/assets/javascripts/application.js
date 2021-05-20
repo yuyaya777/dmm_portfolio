df098bd227f2a9d5f9a020dcc8b4e2b8c2aa39af
yuyaya777 <yuyaya777@gmail.com>
2021-05-20 09:38:29 +0000

    レビューテーブル作成、jquery.raty.jsファイル作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery.raty.js
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_105414) do
+ActiveRecord::Schema.define(version: 2021_05_20_091438) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2021_05_12_105414) do
   create_table "post_favorites", force: :cascade do |t|
     t.integer "member_id"
     t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_reviews", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "post_id"
+    t.float "review", default: 0.0
+    t.text "review_comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
