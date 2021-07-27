49fce11e2ec3383a4fb8d33a26067ab2a7e22c18
yuyaya777 <yuyaya777@gmail.com>
2021-07-27 07:09:16 +0000

    inquiryのコントローラ記述

--- app/controllers/member/inquiry_controller.rb
+++ app/controllers/member/inquiry_controller.rb
@@ -1,10 +1,21 @@
 class Member::InquiryController < ApplicationController
   def index
+    @inquiry = Inquiry.new
+    render :index
   end
 
   def conform
+    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))
+    if @inquiry.valid?
+      render :conform
+    else
+      render :index
+    end
   end
 
   def thanks
+    @inquiry =Inquiry.new(params[:inquiry].permit(:name, :email, :message))
+    InquiryMailer.received_email(@inquiry).deliver
+    render :thanks
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :member
   has_many :post_comments
   has_many :post_favorites
-  
+
   # 投稿物に対してのfavorited_byメソッド
   def favorited_by?(member)
     post_favorites.where(member_id: member.id).exists?
