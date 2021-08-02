7d456af5717ae4e9a30524b1df431d78fed9254d
yuyaya777 <yuyaya777@gmail.com>
2021-08-02 11:15:13 +0000

    mailers/inquiry_mailer.rb記述

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,2 +1,8 @@
 class InquiryMailer < ApplicationMailer
+  default from: "example@example.com"
+
+  def received_email(inquiry)
+    @inquiry = inquiry
+    mail(:to => inquiry.email, :subject => '問い合わせを承りました')
+  end
 end
