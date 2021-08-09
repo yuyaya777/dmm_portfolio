c67f16c071ee91f4d021210bcb7f18021ffcf47f
yuyaya777 <yuyaya777@gmail.com>
2021-08-09 02:01:38 +0000

    メールアドレスとパスワードの環境変数化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            '<gmailのメールアドレス>',
-    password:             '<Googleで設定した、アプリパスワード>',
+    user_name:            ENV['SEND_MAIL'],
+    password:             ENV['SEND_MAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto:  true
   }
